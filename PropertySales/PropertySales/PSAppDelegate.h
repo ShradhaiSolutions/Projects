1994330eabcb10f472d9a75ed6bac9282c8b9e1c
Shradha iSolutions <shradhaisolutions@gmail.com>
2014-02-01 16:55:21 -0500

    PropertySales Initial Commit

---
+++ PropertySales/PropertySales/PSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PSAppDelegate.h
+//  PropertySales
+//
+//  Created by Muddineti, Dhana (NonEmp) on 2/1/14.
+//  Copyright (c) 2014 Shradha iSolutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PropertySales/PropertySales/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PropertySales
+//
+//  Created by Muddineti, Dhana (NonEmp) on 2/1/14.
+//  Copyright (c) 2014 Shradha iSolutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PSAppDelegate class]));
+    }
+}
