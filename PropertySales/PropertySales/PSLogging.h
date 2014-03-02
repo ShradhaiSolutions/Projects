bc14ad0658d7ea5d149e5c996f2e2a9b9e6dc5d2
Shradha iSolutions <shradhaisolutions@gmail.com>
2014-03-01 19:50:29 -0500

    Log Details update

--- PropertySales/PropertySales/PSLogging.h
+++ PropertySales/PropertySales/PSLogging.h
@@ -11,21 +11,15 @@
 
 #import "DDLog.h"
 
-//#define ENTRY_LOG      DDLogDebug(@"%s ENTRY ", __PRETTY_FUNCTION__);
-//#define EXIT_LOG       DDLogDebug(@"%s EXIT ", __PRETTY_FUNCTION__);
-//#define ERROR_EXIT_LOG DDLogError(@"%s ERROR EXIT", __PRETTY_FUNCTION__);
+#ifdef DEBUG
+
+static int ddLogLevel = LOG_LEVEL_DEBUG;
 
 #define ENTRY_LOG      DDLogDebug(@"%s ENTRY. isMainThread - %@", __PRETTY_FUNCTION__, [NSThread isMainThread] ? @"YES" : @"NO");
 #define EXIT_LOG       DDLogDebug(@"%s EXIT. isMainThread - %@", __PRETTY_FUNCTION__, [NSThread isMainThread] ? @"YES" : @"NO");
 #define ERROR_EXIT_LOG DDLogError(@"%s ERROR EXIT. isMainThread - %@", __PRETTY_FUNCTION__, [NSThread isMainThread] ? @"YES" : @"NO");
 
 
-//Compiler does the string concatenation
-//#define LogVerbose(frmt, ...) DDLogVerbose(@"%s: " frmt, __PRETTY_FUNCTION__, ##__VA_ARGS__);
-//#define LogDebug(frmt, ...) DDLogDebug(@"%s: " frmt, __PRETTY_FUNCTION__, ##__VA_ARGS__);
-//#define LogInfo(frmt, ...) DDLogInfo(@"%s: " frmt, __PRETTY_FUNCTION__, ##__VA_ARGS__);
-//#define LogWarn(frmt, ...) DDLogWarn(@"%s: " frmt, __PRETTY_FUNCTION__, ##__VA_ARGS__);
-//#define LogError(frmt, ...) DDLogError(@"%s: " frmt, __PRETTY_FUNCTION__, ##__VA_ARGS__);
 
 #define LogVerbose(frmt, ...) DDLogVerbose(@"%s: isMainThread - %@:  " frmt, __PRETTY_FUNCTION__, [NSThread isMainThread] ? @"YES" : @"NO", ##__VA_ARGS__);
 #define LogDebug(frmt, ...) DDLogDebug(@"%s: isMainThread - %@:  " frmt, __PRETTY_FUNCTION__, [NSThread isMainThread] ? @"YES" : @"NO", ##__VA_ARGS__);
@@ -33,22 +27,25 @@
 #define LogWarn(frmt, ...) DDLogWarn(@"%s: isMainThread - %@:  " frmt, __PRETTY_FUNCTION__, [NSThread isMainThread] ? @"YES" : @"NO", ##__VA_ARGS__);
 #define LogError(frmt, ...) DDLogError(@"%s: isMainThread - %@:  " frmt, __PRETTY_FUNCTION__, [NSThread isMainThread] ? @"YES" : @"NO", ##__VA_ARGS__);
 
-
 //With whether the current thread is Main thread or not
 #define LogDetails(frmt, ...) DDLogInfo(@"%s: isMainThread - %@:  " frmt, __PRETTY_FUNCTION__, [NSThread isMainThread] ? @"YES" : @"NO", ##__VA_ARGS__);
 
-
-#ifdef DEBUG
-static int ddLogLevel = LOG_LEVEL_DEBUG;
 #else
+
 static int ddLogLevel = LOG_LEVEL_ERROR;
-#endif
+
+#define ENTRY_LOG      DDLogDebug(@"%s ENTRY ", __PRETTY_FUNCTION__);
+#define EXIT_LOG       DDLogDebug(@"%s EXIT ", __PRETTY_FUNCTION__);
+#define ERROR_EXIT_LOG DDLogError(@"%s ERROR EXIT", __PRETTY_FUNCTION__);
+
+//Compiler does the string concatenation
+#define LogVerbose(frmt, ...) DDLogVerbose(@"%s: " frmt, __PRETTY_FUNCTION__, ##__VA_ARGS__);
+#define LogDebug(frmt, ...) DDLogDebug(@"%s: " frmt, __PRETTY_FUNCTION__, ##__VA_ARGS__);
+#define LogInfo(frmt, ...) DDLogInfo(@"%s: " frmt, __PRETTY_FUNCTION__, ##__VA_ARGS__);
+#define LogWarn(frmt, ...) DDLogWarn(@"%s: " frmt, __PRETTY_FUNCTION__, ##__VA_ARGS__);
+#define LogError(frmt, ...) DDLogError(@"%s: " frmt, __PRETTY_FUNCTION__, ##__VA_ARGS__);
+
+//With whether the current thread is Main thread or not
+#define LogDetails(frmt, ...) DDLogInfo(@"%s: " frmt, __PRETTY_FUNCTION__, ##__VA_ARGS__);
 
 #endif
-
-//LogVerbose(@"Verbose");
-//LogDebug(@"Debug");
-//LogInfo(@"Info");
-//LogWarn(@"Warn");
-//LogError(@"Error");
-
